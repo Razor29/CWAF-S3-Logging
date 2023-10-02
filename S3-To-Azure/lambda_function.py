4ca5c247480848027909812c9e70d14032913549
Tal Yerushalmi <Razor29@gmail.com>
2023-10-02 16:28:22 +0300

    added format options

--- S3-To-Azure/lambda_function.py
+++ S3-To-Azure/lambda_function.py
@@ -1,11 +1,14 @@
 import boto3
+import gzip
+import json
 import urllib.parse 
 import urllib3
 import certifi
 
+s3_client = boto3.client('s3')
+
 def lambda_handler(event, context):
     # AWS S3 Configuration
-    s3_client = boto3.client('s3')
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     object_key = event['Records'][0]['s3']['object']['key']
     object_key = urllib.parse.unquote_plus(object_key)
@@ -13,25 +16,44 @@ def lambda_handler(event, context):
     print(f"Bucket: {bucket_name}")
     print(f"Key: {object_key}")
 
+    format = "json.gz" # Options are json.gz, ndjson and json
 
     # Get the file from S3
     s3_response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
     file_content = s3_response['Body'].read()
 
+    if format == "ndjson":
+        # Transform JSON.gz to NDJSON
+        data = json.loads(gzip.decompress(file_content).decode('utf-8'))
+        ndjson_content = '\n'.join(json.dumps(item) for item in data)
+        file_content = ndjson_content.encode('utf-8')  # Convert back to bytes for upload
+
+    elif format == "json":
+        # Decompress the JSON.gz to JSON
+        file_content = gzip.decompress(file_content)
+
     # Azure Blob Storage Configuration
-    account_name = 'cwaflogs' # enter the name of the azure storage account
-    container_name = 'logs' # enter the name of the storage account container
+    account_name = '' # enter the name of the azure storage account
+    container_name = '' # enter the name of the storage account container
     blob_name = object_key  # or any other desired name
-    sas_token = ''  # It will look something like "?sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2023-10-01T00:00:00Z&st=2023-10-01T00:00:00Z&spr=https&sig=..."
+    sas_token = ''  # SAS token details
+
     # Create the URL with the SAS token appended
     url = f"https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}{sas_token}"
 
-    # Upload to Azure Blob Storage using REST API
+    # Headers for the request
     headers = {
         'x-ms-blob-type': 'BlockBlob',
         'Content-Type': 'application/json; charset=utf-8',
         'Content-Encoding': 'gzip'
     }
+    if format == "ndjson":
+        headers['Content-Type'] = 'application/x-ndjson'
+        headers.pop('Content-Encoding', None)  # Remove gzip encoding for ndjson
+    elif format == "json":
+        headers.pop('Content-Encoding', None)  # Remove gzip encoding for plain json
+
+
     # Initialize the HTTP client
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
@@ -45,4 +67,3 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': 'File transferred successfully!'
     }
-
