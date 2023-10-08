344b7633a3f87b53d55ecf0b87a0f42f1d8159fb
Tal Yerushalmi <Razor29@gmail.com>
2023-10-08 16:41:56 +0300

    Update lambda_function.py
    Added option to choose export type

--- Elastic-Logstash-Mutations-in-LS/AWS-Lambda/lambda_function.py
+++ Elastic-Logstash-Mutations-in-LS/AWS-Lambda/lambda_function.py
@@ -1,7 +1,7 @@
 import boto3
 import gzip
 import json
-import urllib.parse  
+import urllib.parse  # <-- Import this
 
 s3_client = boto3.client('s3')
 
@@ -10,6 +10,7 @@ DELETE_ORIGINAL = True  # Set to False if you don't want to delete the original 
 SUFFIX_MODE = "remove"  # Modes: "add" or "remove"
 ORIGINAL_SUFFIX = "unprocessed"  # The suffix in the original folder name
 NEW_SUFFIX = ""  # The suffix to add in the new folder name
+OUTPUT_FORMAT = "ndjson"  # Options: "ndjson" or "json"
 
 def lambda_handler(event, context):
     print("Lambda invoked.")
@@ -23,35 +24,48 @@ def lambda_handler(event, context):
 
     # Download the file and read its content
     download_path = '/tmp/{}'.format(key.split('/')[-1])
+    print(f"Downloading file to: {download_path}")
     s3_client.download_file(bucket, key, download_path)
     
     with gzip.open(download_path, 'rt') as f:
         data = json.load(f)
     
-    # Transform JSON to NDJSON
-    ndjson_content = '\n'.join(json.dumps(item) for item in data)
+    print("File contents read successfully.")
     
+    # Transform JSON based on OUTPUT_FORMAT
+    if OUTPUT_FORMAT == "ndjson":
+        transformed_content = '\n'.join(json.dumps(item) for item in data)
+        output_extension = '.ndjson'
+    else:  # Assuming "json"
+        transformed_content = json.dumps(data)
+        output_extension = '.json'
+    print(f"Transformation to {OUTPUT_FORMAT} done.")
+
     # Determine the output path
     first_folder = key.split('/')[0]
     if SUFFIX_MODE == 'remove':
         first_folder = first_folder.replace(f'-{ORIGINAL_SUFFIX}', '')
     elif SUFFIX_MODE == 'add':
         first_folder = f'{first_folder}-{NEW_SUFFIX}'
-        
-    output_key = key.replace(key.split('/')[0], first_folder).replace('.json.gz', '.ndjson')
+
+    # Modify the output path logic
+    output_key = key.replace(key.split('/')[0], first_folder).replace('.json.gz', output_extension)
     output_path = f'/tmp/{output_key.split("/")[-1]}'
-    
-    # Save the transformed content
+    print(f"Writing transformed content to: {output_path}")
+
     with open(output_path, 'w') as f:
-        f.write(ndjson_content)
+        f.write(transformed_content)
     
-    # Upload the transformed content to S3
+    # Upload the transformed content to the different folder in the same S3 bucket
+    print(f"Uploading transformed content to S3 bucket: {bucket} and key: {output_key}")
     s3_client.upload_file(output_path, bucket, output_key)
     
     # Optionally delete the original file
     if DELETE_ORIGINAL:
         s3_client.delete_object(Bucket=bucket, Key=key)
 
+    print("Lambda execution completed.")
+    
     return {
         'statusCode': 200,
         'body': json.dumps('File processed successfully!')
